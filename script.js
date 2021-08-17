2eb715cb8956b1205a84908ed9f4a63fa8f6eac2
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-08-17 13:16:43 +0200

    opdracht klaar

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("test")
+const fetch = require("node-fetch");
+
+fetch("https://swapi.dev/api/people/1/")
+.then(resonse => resonse.json())
+.then(data => console.log(data))
+.catch(err => console.log(err))
